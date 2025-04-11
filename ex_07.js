18ccb13349843a5a704f9d1d3b4a65aa1a75a4d4
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-04-11 11:32:11 -0300

    calcula quantas moedas precisa para juntar pelo menos 12 reais.

--- ex_07.js
+++ ex_07.js
@@ -2,7 +2,7 @@ let moedas = 0;
 let reais = 0;
 
 while (reais < 12) {
-    reais = moedas*3;
+    reais = moedas * 3;
     console.log(`Precisa de  ${moedas} moedas para juntar ${reais} reais`);
     moedas++;
 }
